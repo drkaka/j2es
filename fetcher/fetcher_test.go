318da23a464cc6509b98f8bf7b92bfa57d9681f9
drkaka <drkaka@drkaka.com>
2017-06-30 18:15:40 +0800

    Comment out the fetcher test method.

--- fetcher/fetcher_test.go
+++ fetcher/fetcher_test.go
@@ -1,22 +1,15 @@
 package fetcher
 
-import (
-	"fmt"
-	"testing"
+// func TestGetMessage(t *testing.T) {
+// 	lg.InitLogger(true)
 
-	"github.com/drkaka/lg"
-)
+// 	jCmd := fmt.Sprintf("journalctl -u hooks -o json -n 20")
+// 	results, err := GetMessages("hooks", "ssh", "leeq@192.168.1.201", jCmd)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-func TestGetMessage(t *testing.T) {
-	lg.InitLogger(true)
-
-	jCmd := fmt.Sprintf("journalctl -u hooks -o json -n 20")
-	results, err := GetMessages("hooks", "ssh", "leeq@192.168.1.201", jCmd)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	for _, one := range results {
-		fmt.Println("Message: ", string(one.Message))
-	}
-}
+// 	for _, one := range results {
+// 		fmt.Println("Message: ", string(one.Message))
+// 	}
+// }
